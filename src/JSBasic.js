1567963909e689b69407c0bf90acc0b5e4e632b2
Mohit Gupta <mohit.gupta2jan@gmail.com>
2021-08-06 22:48:42 +0530

    Add files via upload

---
+++ src/JSBasic.js
@@ -0,0 +1,29 @@
+function changeText() {
+    // Function to change heading text...
+    var heading_1 = document.getElementById("heading_1");
+    heading_1.innerHTML = "The Text is changed";
+}
+
+function resetText() {
+    // Function to reset heading text...
+    var heading_1 = document.getElementById("heading_1");
+    heading_1.innerHTML = "This is a heading.";
+}
+
+function redText() {
+    // Function to make heading text red...
+    var heading_1 = document.getElementById("heading_1");
+    heading_1.style.color = "red"
+}
+
+function greenText() {
+    // Function to make heading text green...
+    var heading_1 = document.getElementById("heading_1");
+    heading_1.style.color = "green"
+}
+
+function blueText() {
+    // Function to make heading text blue...
+    var heading_1 = document.getElementById("heading_1");
+    heading_1.style.color = "blue"
+}
